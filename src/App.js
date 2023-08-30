e0c4e34a79b02dbfdd5abd846f16cf0ba07d146c
karthikprasannamuralidharan <karthikprasannamuralidharan@gmail.com>
2023-08-30 20:45:30 +0530

    tailwind

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h2 className='text-lime-400'>
+        Hello Karthik
+      </h2>
+    </div>
+  );
+}
+
+export default App;
